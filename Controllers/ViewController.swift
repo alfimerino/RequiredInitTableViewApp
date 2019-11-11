656b8b03f76b448166d32c602f7cc792c72ffb0e
Freddy <alfimerino@gmail.com>
2019-11-11 14:01:28 -0800

    - Created setupUI function to create tableView constraints

--- Controllers/ViewController.swift
+++ Controllers/ViewController.swift
@@ -9,9 +9,11 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var ints = [1, 2, 3]
+    override var title: String? = "Williams"
     private lazy var tableView: UITableView = {
         let tableView = UITableView()
+        
         tableView.translatesAutoresizingMaskIntoConstraints = false
         tableView.rowHeight = UITableView.automaticDimension
         tableView.estimatedRowHeight = 80
@@ -21,7 +23,21 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        // make tableview show up on screen
+        setupUI() //  P1 P1
+        // pinned to edges
+    }
+    
+    func setupUI() {
+        view.addSubview(tableView)
+        view.addConstraints([
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
     }
 }
+
+//sextension ViewController: view
 
